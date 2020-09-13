76404cdd4c3da6285a8f7bdfa0f234abe361d65f
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2020-09-13 21:32:40 +0200

    Updating console gui.

--- Console Calculator/main.py
+++ Console Calculator/main.py
@@ -18,28 +18,41 @@ def multiplicate(a, b):
     return a * b
 
 console_gui = """
-Welcome to simple console calculator by Mateusz Hyla.
-Available actions: add, subtract, divide, multiplicate or quit.
-Type one of this actions.
+====================================================================
+|  Welcome to simple console calculator by Mateusz Hyla.           |
+|  Available actions:                                              |
+|       actions           - show available actions.                |
+|       add               - ads two numbers.                       |
+|       subtract          - subrast two numbers.                   |
+|       divide            - divide two numbers                     |
+|       multiplicate      - multiplicate two numbers               |
+|       quit                                                       |
+|  Type one of this actions to start                               |
 """
 
-print(console_gui)
+available_actions = ["actions", "add", "subract", "divide", "multiplicate", "quit"]
 
-result = calculate(add, 5, 5)
-print(result)
+print(console_gui)
 #
-# while True:
-#     action = input("You action is: ")
-#     a = int(input("First number is: "))
-#     b = int(input("second number is: "))
-#
-#     if action == "quit" or action == "q":
-#         print("Thank you for your time. Bye.")
-#         break
-#
-#     try:
-#         result = calculate(action, a, b)
-#     except TypeError:
-#         print("Mismatch in types. Contact admin.")
-#       except NameError:
-#           print("That action is not allowed.")
+# result = calculate(divide, 5, 0)
+# print(result)
+
+while True:
+    action = input("You action is: ")
+    a = int(input("First number is: "))
+    b = int(input("Second number is: "))
+
+    if action == "quit" or action == "q":
+        print("Thank you for your time. Bye.")
+        break
+
+    try:
+        result = calculate(action, a, b)
+    except TypeError:
+        print("Mismatch in types. Contact admin.")
+    except NameError:
+        print("That action is not allowed.")
+    except ZeroDivisionError:
+        print("Second number cannot be 0 if you pick division.")
+
+
