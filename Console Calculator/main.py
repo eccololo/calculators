2bee8f6cc7bf25e5e37b58cd1ee51a9990a717ab
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2020-09-17 23:56:47 +0200

    Adding dictionary with objects which contain all info about available actions. Encountering a problem with accessing data from it. Problem under solving process.

--- Console Calculator/main.py
+++ Console Calculator/main.py
@@ -2,22 +2,16 @@ from termcolor import colored, cprint
 
 email = colored('mstem.net@gmail.com', 'yellow', attrs=['bold'])
 
-available_actions = ["actions", "add", "subtract", "divide", "multiplicate", "quit"]
-available_descriptions = [
-    "- show available actions.",
-    "- ads two numbers.",
-    "- subrast two numbers.",
-    "- divide two numbers.,",
-    "- multiplicate two numbers.",
-    "- type quit or q to exit."
-]
-actions_colored = []
-for action in available_actions:
-    color = "yellow"
-    if action == "quit":
-       color = "red"
-    actions_colored.append(colored(action, color, attrs=['bold']))
 
+# available_actions = ["actions", "add", "subtract", "divide", "multiplicate", "quit"]
+# available_descriptions = [
+#     "- show available actions.",
+#     "- ads two numbers.",
+#     "- subrast two numbers.",
+#     "- divide two numbers.",
+#     "- multiplicate two numbers.",
+#     "- type quit or q to exit."
+# ]
 
 
 def calculate(operation, a, b):
@@ -62,6 +56,56 @@ def multiplicate(a, b):
     return a * b
 
 
+calculator_objects = {
+    "add": {
+        "method": add,
+        "symbol": "+",
+        "gui_action": "add",
+        "description": "- ads two numbers.",
+        "color": "yellow"
+    },
+    "subtract": {
+        "method": subtract,
+        "symbol": "-",
+        "gui_action": "subtract",
+        "description": "- subrast two numbers.",
+        "color": "yellow"
+    },
+    "divide": {
+        "method": divide,
+        "symbol": "/",
+        "gui_action": "divide",
+        "description": "- divide two numbers.",
+        "color": "yellow"
+    },
+    "multiplicate": {
+        "method": multiplicate,
+        "symbol": "*",
+        "gui_action": "mult",
+        "description": "- multiplicate two numbers.",
+        "color": "yellow"
+    },
+    "quit": {
+        "method": None,
+        "symbol": None,
+        "gui_action": "quit",
+        "description": "- type quit or q to exit.",
+        "color": "red"
+    },
+    "actions": {
+        "method": None,
+        "symbol": None,
+        "gui_action": "actions",
+        "description": "- show available actions.",
+        "color": "blue"
+    }
+}
+
+actions_colored = []
+for action in calculator_objects.keys():
+    print(action)
+    actions_colored.append(colored(action.get("gui_action"), action.get("color"), attrs=['bold']))
+
 console_gui = f"""
 ====================================================================
 |  Welcome to simple console calculator by Mateusz Hyla.           |
@@ -103,4 +147,5 @@ while True:
     except ZeroDivisionError:
         print("fSecond number cannot be 0 if you pick division. For more info contact admin at {email}.")
     except KeyError:
-        print(f"Internal error of a function. You probably type wrong action: {action_colored}. Contact admin at {email}.")
+        print(
+            f"Internal error of a function. You probably type wrong action: {action_colored}. Contact admin at {email}.")
