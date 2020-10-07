5e14aa51af891da3262fd9930201bfac5e3d6e47
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2020-10-07 15:02:21 +0200

    Creating plus button and add function.

--- Window App Calculator/main.py
+++ Window App Calculator/main.py
@@ -5,10 +5,17 @@ def action_result():
     pass
 
 
+def add():
+    pass
+
+
 root = Tk()
 
-result_button = Button(root, text="=", command=action_result, bg="gray", padx=40, pady=40)
+plus_button = Button(root, text="+", command=add, padx=20, pady=20)
+plus_button.grid(row=4, column=0)
 
-result_button.grid(row=5, column=0)
+
+result_button = Button(root, text="=", command=action_result, bg="gray", padx=40, pady=40)
+result_button.grid(row=5, column=0, columnspan=2, rowspan=2)
 
 root.mainloop()
