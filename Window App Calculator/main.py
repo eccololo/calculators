9dd62788c77b6c22f422f6e38f5901bc9e60ef4b
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2020-10-07 14:58:35 +0200

    Creating result button.

--- Window App Calculator/main.py
+++ Window App Calculator/main.py
@@ -1,7 +1,14 @@
 from tkinter import *
 
+
+def action_result():
+    pass
+
+
 root = Tk()
 
+result_button = Button(root, text="=", command=action_result, bg="gray", padx=40, pady=40)
 
+result_button.grid(row=5, column=0)
 
 root.mainloop()
