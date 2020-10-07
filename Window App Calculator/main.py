ac3fc5ee9fcabcb69812b88484d496f41303ef95
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2020-10-07 15:09:32 +0200

    Making constants for small buttons sizing for DRY rule.

--- Window App Calculator/main.py
+++ Window App Calculator/main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+Y_SMALL_BTN_SIZE = 18
+X_SMALL_BTN_SIZE = 20
 
 def action_result():
     pass
@@ -17,16 +19,23 @@ def mult():
     pass
 
 
+def divide():
+    pass
+
+
 root = Tk()
 
-plus_button = Button(root, text="+", command=add, padx=20, pady=20)
+plus_button = Button(root, text="+", command=add, padx=X_SMALL_BTN_SIZE, pady=Y_SMALL_BTN_SIZE)
 plus_button.grid(row=4, column=0)
 
-minus_button = Button(root, text="-", command=minus, padx=20, pady=20)
+minus_button = Button(root, text="-", command=minus, padx=X_SMALL_BTN_SIZE, pady=Y_SMALL_BTN_SIZE)
 minus_button.grid(row=4, column=1)
 
-mult_button = Button(root, text="*", command=mult, padx=20, pady=20)
+mult_button = Button(root, text="*", command=mult, padx=X_SMALL_BTN_SIZE, pady=Y_SMALL_BTN_SIZE)
 mult_button.grid(row=5, column=2)
+
+divide_button = Button(root, text="/", command=divide, padx=X_SMALL_BTN_SIZE, pady=Y_SMALL_BTN_SIZE)
+divide_button.grid(row=6, column=2)
 
 result_button = Button(root, text="=", command=action_result, bg="gray", padx=46, pady=46)
 result_button.grid(row=5, column=0, columnspan=2, rowspan=2)
